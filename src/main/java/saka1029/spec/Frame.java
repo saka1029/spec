555f3aba9493e0ce17c7a4c8be8fa7e6915d12c7
saka1029 <saka1029@gmail.com>
2024-09-17 16:00:14 +0900

    ..

--- src/main/java/saka1029/spec/Frame.java
+++ src/main/java/saka1029/spec/Frame.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.ListIterator;
 import java.util.Map;
 
-public class Frame {
+public class Frame implements Instruction {
 
     final java.util.List<Symbol> inputs = new ArrayList<>(), outputs = new ArrayList<>();
     final Map<Symbol, Integer> locals = new HashMap<>();
@@ -15,6 +15,17 @@ public class Frame {
         int pos = -1;
         for (ListIterator<Symbol> i = arguments.listIterator(arguments.size()); i.hasPrevious(); )
             locals.put(i.next(), pos--);
+    }
+
+    int addLocal(Symbol name) {
+        locals.put(name, localOffset);
+        return localOffset++;
+    }
+
+    @Override
+    public void execute(Context context) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
     }
 
 }
