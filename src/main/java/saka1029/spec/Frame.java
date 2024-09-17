bac547a657f598227f1fd0a6fe8c17cdc4399cbb
saka1029 <saka1029@gmail.com>
2024-09-17 15:49:03 +0900

    ..

--- src/main/java/saka1029/spec/Frame.java
+++ src/main/java/saka1029/spec/Frame.java
@@ -1,5 +1,20 @@
 package saka1029.spec;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.ListIterator;
+import java.util.Map;
+
 public class Frame {
+
+    final java.util.List<Symbol> inputs = new ArrayList<>(), outputs = new ArrayList<>();
+    final Map<Symbol, Integer> locals = new HashMap<>();
+    int offset = 0;
+
+    Frame(java.util.List<Symbol> arguments) {
+        int pos = -1;
+        for (ListIterator<Symbol> i = arguments.listIterator(arguments.size()); i.hasPrevious(); )
+            locals.put(i.next(), pos--);
+    }
 
 }
