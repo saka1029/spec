2266ee6c2a421b136188dc0b521a912944867f09
saka1029 <saka1029@gmail.com>
2024-10-15 15:55:13 +0900

    ..

--- src/test/java/saka1029/spec/TestParser.java
+++ src/test/java/saka1029/spec/TestParser.java
@@ -8,12 +8,22 @@ public class TestParser {
 
     @Test
     public void testParse() {
-        Parser parser = new Parser();
-        assertEquals(list(i(1), i(2), s("+")), parser.parse("1 2 +"));
+        Context c = Context.of();
+        Instruction inst = new Parser().parse("1 2 +");
+        assertEquals(list(i(1), i(2), s("+")), inst);
+        assertEquals(i(3), c.eval(inst));
     }
 
     @Test
     public void testList() {
+        Context c = Context.of();
+        Instruction inst = new Parser().parse("(1 2 +)");
+        assertEquals(list(list(i(1), i(2), s("+"))), inst);
+        assertEquals(i(3), c.eval(inst));
+    }
+
+    @Test
+    public void testIf() {
         Parser parser = new Parser();
         assertEquals(list(list(i(1), i(2), s("+"))), parser.parse("(1 2 +)"));
     }
